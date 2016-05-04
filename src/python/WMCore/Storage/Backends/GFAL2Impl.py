0537d8164b600cc8cb6f8a8b782a79876093e374
Emilis Rupeika <phonixreal@gmail.com>
2016-05-04 10:21:08 +0200

    Merge Justas' patch for gfal2 fix

--- src/python/WMCore/Storage/Backends/GFAL2Impl.py
+++ src/python/WMCore/Storage/Backends/GFAL2Impl.py
@@ -21,12 +21,12 @@ class GFAL2Impl(StageOutImpl):
     def __init__(self, stagein=False):
         StageOutImpl.__init__(self, stagein)
         # If we want to execute commands in clean shell, we can`t separate them with ';'.
-        # Next commands after separation are executed without env -i and this leads us with
+        # Next commands after separation are executed without env -i and this leads us with 
         # mixed environment with COMP and system python.
         # GFAL2 is not build under COMP environment and it had failures with mixed environment.
-        self.setups = "env -i X509_USER_PROXY=$X509_USER_PROXY bash -c '%s'"
-        self.removeCommand = self.setups % '. $PWD/startup_environment.sh; printenv; date; gfal-rm -vvv -t 600 %s '
-        self.copyCommand = self.setups % '. $PWD/startup_environment.sh; printenv; date; gfal-copy -vvv -t 2400 -T 2400 -p %(checksum)s %(options)s %(source)s %(destination)s'
+        self.setups = "env -i X509_USER_PROXY=$X509_USER_PROXY sh -c '%s'"
+        self.removeCommand = self.setups % 'printenv; date; gfal-rm -vvv -t 600 %s '
+        self.copyCommand = self.setups % 'printenv; date; gfal-copy -vvv -t 2400 -T 2400 -p %(checksum)s %(options)s %(source)s %(destination)s'
 
     def createSourceName(self, protocol, pfn):
         """
@@ -71,14 +71,15 @@ class GFAL2Impl(StageOutImpl):
 
         useChecksum = (checksums != None and 'adler32' in checksums and not self.stageIn)
 
-        copyCommand = self.copyCommand
+        copyCommandDict = {'checksum': '', 'options': '', 'source': '', 'destination': ''}
         if useChecksum:
-            copyCommand += " -K adler32 "
+            copyCommandDict['checksum'] = "-K adler32"
         if options != None:
-            copyCommand += " %s " % options
-        copyCommand += " %s " % sourcePFN
-        copyCommand += " %s \n" % targetPFN
+            copyCommandDict['options'] = options
+        copyCommandDict['source'] = sourcePFN
+        copyCommandDict['destination'] = targetPFN
 
+        copyCommand = self.copyCommand % copyCommandDict
         result += copyCommand
 
         if _CheckExitCodeOption:
